bf0c0016cf1073f2a3787bec701d507120c8caa8
Muyale Okoyo <annmuyale@gmail.com>
2023-03-31 15:41:24 +0300

    first commit

---
+++ mathh.py
@@ -0,0 +1,22 @@
+def add(a,b):
+    answer=a+b
+    return  answer
+
+def minus(c,d):
+    result =c-d
+    return result
+
+
+def divide(e,f):
+    get =e/f
+    return get
+    
+def times(g,h):
+    give =g*h
+    return give
+
+def remainder(i,j):
+    receive=i%j
+    return receive 
+
+        
